124f07971ec376788477765861e7944d58d45008
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-10-04 00:37:37 +0530

    Added the coctails section

--- components/Cocktails.jsx
+++ components/Cocktails.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import { cocktailLists, mockTailLists } from '../constants/index'
+import gsap from 'gsap'
 
 const Cocktails = () => {
+    useGSAP(() => {
+        const parallaxTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#cocktails',
+                start: 'top 30%',
+                end: 'bottom 80%',
+                scrub: true,
+            }
+        })
+
+        parallaxTimeline
+            .from('#c-left-leaf', {
+                x: -100, y: 100
+            })
+            .from('#c-right-leaf', {
+                x: 100, y: 100
+            })
+    })
+
   return (
-    <div>
-      <h2>Cocktails</h2>
-      <p>Explore our exquisite cocktail menu, featuring a variety of flavors and styles.</p>
-    </div>
+    <section id='cocktails' className='noisy'>
+        <img src='/images/cocktail-left-leaf.png' alt='l-leaf' id='c-left-leaf' />
+        <img src='/images/cocktail-right-leaf.png' alt='r-leaf' id='c-right-leaf' />
+
+        <div className='list'>
+            <div className='popular'>
+                <h2>Our Popular Cocktails:</h2>
+
+                <ul>
+                    {cocktailLists.map(({ name, country, detail, price }) => (
+                        <li key={name}>
+                            <div className='me-28'>
+                                <h3>{name}</h3>
+                                <p>{country} | {detail}</p>
+                            </div>
+                            <span>{price}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className='loved'>
+                <h2>Our Loved Mocktails:</h2>
+
+                <ul>
+                    {mockTailLists.map(({ name, country, detail, price }) => (
+                        <li key={name}>
+                            <div className='md:me-28'>
+                                <h3>{name}</h3>
+                                <p>{country} | {detail}</p>
+                            </div>
+                            <span>{price}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    </section>
   )
 }
 
