ef5536ddaa2358725b854ea61c03956f3ca6ad50
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-10-03 21:06:41 +0530

    first commit

---
+++ components/Cocktails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cocktails = () => {
+  return (
+    <div>
+      <h2>Cocktails</h2>
+      <p>Explore our exquisite cocktail menu, featuring a variety of flavors and styles.</p>
+    </div>
+  )
+}
+
+export default Cocktails
