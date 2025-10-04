5659dc136956f26bcdbc947512315d654a4e213a
Abhijeet Sharan <abhijeetsharan0007@gmail.com>
2025-10-04 22:19:53 +0530

    Implemented contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
 import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -15,6 +17,8 @@ const App = () => {
         <Cocktails />
         <About />
         <Art />
+        <Menu />
+        <Contact />
     </main>
   )
 }
